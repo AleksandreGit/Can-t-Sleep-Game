db53a977846c17f674d7bc27d58dc7b8f10657d2
Alexandre <achapin@insa-rennes.fr>
2021-01-13 11:11:43 +0100

    Define VoidAnimableBuilder

--- GameJam1/VoidAnimableBuilder.cpp
+++ GameJam1/VoidAnimableBuilder.cpp
@@ -1,1 +1,40 @@
 #include "VoidAnimableBuilder.h"
+
+VoidAnimableBuilder::VoidAnimableBuilder() {}
+
+AnimableEntity VoidAnimableBuilder::build() {
+	return AnimableEntity(texture, spriteSheet, frameRate, framePerCol, framePerLig, totalFrame);
+}
+
+void VoidAnimableBuilder::setSpriteSheet(string path) {
+	if (!texture.loadFromFile(path)) {
+		cout << "Erreur de chargement de la texture !" << endl;
+	}
+	const int width = texture.getSize().x / framePerCol;
+	const int height = texture.getSize().y / framePerLig;
+	for (int i = 0; i < totalFrame; i++) {
+		sf::Sprite sprite;
+		sprite.setTexture(texture);
+		sprite.setTextureRect(sf::IntRect((i % framePerCol) * width,
+			(i / framePerLig) * height,
+			width,
+			height));
+		spriteSheet.push_back(sprite);
+	}
+}
+
+void VoidAnimableBuilder::setFrameRate(int frameRate) {
+	this->frameRate = frameRate;
+}
+
+void VoidAnimableBuilder::setFrameRatePerCol(int frameRatePerCol) {
+	this->framePerCol = framePerCol;
+}
+
+void VoidAnimableBuilder::setFrameRatePerLig(int frameRatePerLig) {
+	this->framePerLig = framePerLig;
+}
+
+void VoidAnimableBuilder::setTotalFrame(int totalFrame) {
+	this->totalFrame = totalFrame;
+}
--- GameJam1/VoidAnimableBuilder.h
+++ GameJam1/VoidAnimableBuilder.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "AnimableBuilder.h"
+#include <iostream>
 #include <vector>
 #include <string>
 #include <SFML/Graphics.hpp>
@@ -14,6 +15,11 @@ public:
 	VoidAnimableBuilder();
 	virtual AnimableEntity build();
 
+	/**
+		Set the spriteSheet
+		/!\ Muste define the number total of frames, the number
+		of frames per columns and per rows before
+	*/
 	void setSpriteSheet(string path);
 	void setFrameRate(int frameRate);
 	void setFrameRatePerCol(int frameRatePerCol);
@@ -21,6 +27,7 @@ public:
 	void setTotalFrame(int totalFrame);
 
 protected:
+	sf::Texture texture;
 	vector<sf::Sprite> spriteSheet;
 	int frameRate;
 	int framePerCol;
