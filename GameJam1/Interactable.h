e99fa1c51626703a0ab5371259ee73225ef24e67
Alexandre <achapin@insa-rennes.fr>
2021-01-30 23:10:09 +0100

    Begin interaction part

---
+++ GameJam1/Interactable.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "RigidElement.h"
+
+class Interactable : public RigidElement
+{
+};
+
---
+++ GameJam1/MovingEntity.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Entity.h"
+
+class MovingEntity : public Entity
+{
+};
+
